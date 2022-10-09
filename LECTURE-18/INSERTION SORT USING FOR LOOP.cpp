78260d1902175f32ca32c72f114e973e000e2ade
Harsh Rawat <112020551+Rawharsh@users.noreply.github.com>
2022-10-09 12:31:10 +0530

    Create INSERTION SORT USING FOR LOOP.cpp

---
+++ LECTURE-18/INSERTION SORT USING FOR LOOP.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[7]={10,1,7,4,8,2,11};
+	for(int i=1;i<7;i++)
+	{
+	   int temp=arr[i];
+	    int j=i-1;
+	    for(;j>=0;j--)
+	    {
+	        if(arr[j]>temp)
+	        {
+	            arr[j+1]=arr[j];
+	        }
+	        else
+	        {
+	            break;
+	        }
+	    }
+	        arr[j+1]=temp;
+	    
+	}
+	for(int j=0;j<7;j++)
+	{
+	    cout<<arr[j]<<" ";
+	}
+	return 0;
+}
